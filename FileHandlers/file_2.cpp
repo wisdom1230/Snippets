68da94a01c25759093151f5a4019b499a7546d17
wisdom1230 <694961699@qq.com>
2022-10-02 21:33:58 +0800

    基本的读取文件内容

---
+++ FileHandlers/file_2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<string>
+#include<sstream>
+
+using namespace std;
+
+
+struct information
+{
+    string coutry;
+    int age;
+    int salay;
+    string purchased;
+};
+
+int main(){
+    char line[1024] {};
+    vector<information> Information;
+
+
+    return 0;
+}
