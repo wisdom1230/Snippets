8b35538ee0432b546a056010bd37a576568017d8
wisdom1230 <694961699@qq.com>
2022-10-03 01:18:30 +0800

    读取csv文件至结构体, 存放值vector中, 然后显示

--- FileHandlers/file_2.cpp
+++ FileHandlers/file_2.cpp
@@ -6,7 +6,6 @@
 
 using namespace std;
 
-
 struct information
 {
     string coutry;
@@ -16,9 +15,37 @@ struct information
 };
 
 int main(){
-    char line[1024] {};
+    auto fileName = "test.txt";
+    fstream file_2;
+    file_2.open(fileName, ios::in);
+    if (!file_2.is_open())
+    {
+        return -1;
+    }
+    
+    string line;
     vector<information> Information;
-
-
+    getline(file_2, line);
+    while (getline(file_2, line))
+    {
+        stringstream ss(line);
+        string data;
+        information info;
+        
+        getline(ss, data, ',');
+        info.coutry = data;
+        getline(ss, data, ',');
+        info.age = atoi(data.c_str());
+        getline(ss, data, ',');
+        info.salay = atoi(data.c_str());
+        getline(ss, data, ',');
+        info.purchased = data;
+        Information.push_back(info);
+    }
+    for (size_t i = 0; i < Information.size(); i++)
+    {
+        cout << Information[i].coutry << "\t" << Information[i].age << "\t" << Information[i].salay << "\t" << Information[i].purchased << endl;
+    }
+    
     return 0;
 }
